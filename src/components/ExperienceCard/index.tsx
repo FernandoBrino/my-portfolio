86c254324afc5bbf00f8769abd84616077cbc2a8
Fernando Brino <fernandobrino11@gmail.com>
2023-10-17 16:33:24 -0300

    style: Experience card

--- src/components/ExperienceCard/index.tsx
+++ src/components/ExperienceCard/index.tsx
@@ -14,13 +14,13 @@ interface Props {
 
 export const ExperienceCard: FC<Props> = ({ job }) => {
   return (
-    <div className="flex p-8 bg-card-background gap-12 rounded-xl">
+    <div className="flex p-8 bg-card-background gap-12 rounded-xl items-center shadow-lg">
       <div>
         <Image src={job.image} alt="" />
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 w-full">
         <div className="flex justify-between items-center">
-          <h3 className="text-title text-xl font-semibold">{job.title}</h3>
+          <h1 className="text-title text-xl font-semibold">{job.title}</h1>
           <p className="text-text text-sm">{job.period}</p>
         </div>
         <div className="ml-5">
