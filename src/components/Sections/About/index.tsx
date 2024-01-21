ee85259d273dd1fa499ea705ef611efd4fc43975
Fernando Brino <fernandobrino11@gmail.com>
2024-01-20 22:42:11 -0300

    fix: import about

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Label } from "../Label";
+import { Label } from "../../Label";
 import aboutProfile from "@/assets/about_profile.jpg";
 
 export const AboutSection = () => (
