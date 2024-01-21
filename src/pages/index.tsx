5f9909fca51e823b0ee91e56f7e7b8a3bdacaa24
Fernando Brino <fernandobrino11@gmail.com>
2024-01-20 22:39:11 -0300

    fix: import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Header } from "@/components/Header";
-import { AboutSection } from "@/components/Sections/About";
-import { ContactSection } from "@/components/Sections/Contact";
-import { ExperienceSection } from "@/components/Sections/Experience";
-import { ProfileSection } from "@/components/Sections/Profile";
-import { ProjectSection } from "@/components/Sections/Project";
-import { RecommendationSection } from "@/components/Sections/Recommendation";
-import { SkillSection } from "@/components/Sections/Skill";
-import { MenuMobileContext } from "@/contexts/MenuMobileContext";
+import { Header } from "../components/Header";
+import { AboutSection } from "../components/Sections/About";
+import { ContactSection } from "../components/Sections/Contact";
+import { ExperienceSection } from "../components/Sections/Experience";
+import { ProfileSection } from "../components/Sections/Profile";
+import { ProjectSection } from "../components/Sections/Project";
+import { RecommendationSection } from "../components/Sections/Recommendation";
+import { SkillSection } from "../components/Sections/Skill";
+import { MenuMobileContext } from "../contexts/MenuMobileContext";
 import { useContext } from "react";
 
 export default function Home() {
